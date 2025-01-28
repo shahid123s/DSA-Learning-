52e28aa9817f437f1f05ac7fa678e8417affb096
shahid123s <shahidnoushad13s@gmail.com>
2025-01-28 13:48:14 +0530

    third question asnwer

--- anagram.js
+++ anagram.js
@@ -1,2 +1,12 @@
 // Question Number 3 - Valid  Anagram
-// 
+//  two words are made by same letter or not  if it is same then it anagram else it not ;
+//  leetcode question Number - 242
+
+const isAnagram = (s, t) => {
+    s = s.split('').sort().join('');
+    t = t.split('').sort().join('');
+
+    return s === t
+}
+
+console.log(isAnagram('shahid', 'dihah')) 
